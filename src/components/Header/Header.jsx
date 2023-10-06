685407704dc975d6307f2d9ae71875f636073cdf
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2023-10-06 09:55:23 +0300

    Add dynamic cart size and cost functions to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Link from 'next/link';
+import productUIService from '@/services/productUI.service';
 
 function Header() {
+  const [cartSize, setCartSize] = useState(0);
+  const [totalCost, setTotalCost] = useState(0);
+
+  useEffect(() => {
+    const size = productUIService.takeCartSize();
+    setCartSize(size);
+
+    const cost = productUIService.takeCartTotalCost();
+    setTotalCost(cost);
+
+    productUIService.takeCartProducts();
+  
+
+  }, [])
+  
 
   return (
     <div className="w-full fixed top-0 z-50 h-[80px]">
@@ -23,13 +40,13 @@ function Header() {
           <Link href="/shopping-cart" className="flex flex-row whitespace-nowrap gap-5 items-center">
             <div>
               <div className="w-5 h-5 bg-orange-600 rounded-full border-2 border-white absolute ml-3 -mt-2 justify-start">
-                <span className="text-xs absolute ml-[3px] -mt-1/2 text-white">0</span>
+                <span className="text-xs absolute ml-[3px] -mt-1/2 text-white">{cartSize}</span>
               </div>
               <ShoppingCartOutlinedIcon color='black' />
             </div>
             <div className="flex flex-col gap-1">
               <span className="text-sm">My Cart</span>
-              <span className="text-xs font-semibold text-orange-700"> $ 500</span>
+              <span className="text-xs font-semibold text-orange-700"> $ {totalCost}</span>
             </div>
           </Link>
         </div>
