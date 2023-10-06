25348550934913322a7d8e79a49d2ba81efc04af
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2023-10-06 03:14:41 +0300

    Rename Shopping Cart filename

--- src/pages/shopping-cart/shopping-cart.jsx
+++ src/pages/shopping-cart/shopping-cart.jsx
@@ -1,3 +1,4 @@
+"use client"
 import BackToHome from '@/components/BackToHome'
 import React from 'react'
 import CartItem from '@/components/CartItem'
@@ -6,14 +7,14 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 function ShoppingCart() {
   return (
     <>
-      <div className="w-full h-full flex flex-col py-[80px] gap-5">
+      <div className="w-full h-full flex flex-col py-[80px] gap-5 px-page">
         <BackToHome />
         <div className="flex flex-row items-center" >
           <span className='text-xl flex-grow'>Cart Summary</span>
           <div className="flex flex-row gap-2 items-center">
             <span>Total Cost:</span>
             <span className='text-orange-600 font-semibold'>$ 4.500</span>
-            <button className="flex flex-row items-center justify-center p-1 text-orange-600 rounded-lg bg-orange-100 hover:text-orange-100 hover:bg-orange-600 transition-colors duration-200 text-sm gap-2">
+            <button className="flex flex-row items-center justify-center py-1 px-3 text-orange-600 rounded-lg bg-orange-100 hover:text-orange-100 hover:bg-orange-600 transition-colors duration-200 text-sm gap-2">
               <FileDownloadOutlinedIcon className='w-[20px]'/>
               <span>Download</span>
             </button>
