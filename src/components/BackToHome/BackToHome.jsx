cb728c6f14841666476202e306c6a9b265347bd4
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2023-10-06 03:18:31 +0300

    Fixed back to home button and add navigation

--- src/components/BackToHome/BackToHome.jsx
+++ src/components/BackToHome/BackToHome.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import Link from 'next/link';
 
 function BackToHome() {
   return (
     <>
-      <div className='flex flex-row gap-5 items-center'>
+      <Link href="/" className='flex flex-row gap-5 items-center'>
         <button type='button' className='bg-orange-100 rounded-full w-[40px] h-[40px] flex items-center justify-center text-orange-600 hover:text-orange-100 hover:bg-orange-600 transition-colors duration-200'>
           <ArrowBackOutlinedIcon />
         </button>
-        <span className="text-[#2f294d]">Back to previous page</span>
-      </div>
+        <span className="text-[#2f294d]">Back to home page</span>
+      </Link>
       <hr />
     </>
   )
