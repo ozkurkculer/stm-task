ad74652a0dfefbd540697f988db2c327333e0e6c
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2023-10-06 03:18:51 +0300

    Make products page dynamically

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -6,6 +6,7 @@ import productUIService from '@/services/productUI.service';
 
 function Products() {
     const [products, setProducts] = useState(``);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         getProducts();
@@ -13,14 +14,15 @@ function Products() {
 
     const getProducts = async () => {
         const data = await productUIService.takeProducts();
-        console.log(data);
+        setData(data);
+        console.log(data[0].images[0]);
         setProducts(data.map((data, index) => {
             return <ProductCard data={data} key={index} />
         }))
     }
 
     return (
-        <div className="w-full h-full flex flex-col py-[80px] gap-5">
+        <div className="w-full h-full flex flex-col py-[80px] gap-5 px-page">
             <BackToHome />
             <h1 className='text-3xl font-bold text-orange-600'>All Products</h1>
             <div className="flex flex-row items-center gap-[25px] flex-wrap justify-center">
